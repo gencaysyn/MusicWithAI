8fcbdf09dd6191c6ccad862bb9cb4d60e3c1b5e3
Gencay <gencaysyn@gmail.com>
2019-04-25 04:54:28 +0300

    new

--- NoteToTxtConverterV2.py
+++ NoteToTxtConverterV2.py
@@ -11,13 +11,23 @@ def noteToTxtConverter(in_file_name, out_file_name):
     result = []
     noteON = "Note_on_c, 0, "
     noteOFF = "Note_off_c, 0, "
-    repeat = []
-
-    for i in range(0, 127):
-        repeat.append(0)
     # for note in data:
     #   result += note.split(" ")
-    result = data
+    result = []
+    print(data)
+    i = 0
+    while i < len(data):
+        if data[i] == " ":
+            result.append(" ")
+        else:
+            j = i
+            result.append("")
+            while data[j] != " ":
+                result[-1] += data[j]
+                j += 1
+            i = j-1
+        i += 1
+    print(data)
     print(result)
     resolution = 512
     final.write("0, 0, Header, 1, 2, " + str(resolution) + "\n")
@@ -29,32 +39,41 @@ def noteToTxtConverter(in_file_name, out_file_name):
     final.writelines("2, 0, Start_track\n")
     final.writelines("2, 0, Title_t, 'Instrument Track 01'\n")
     final.writelines("2, 0, Program_c, 6, 6\n")
+
+    repeat = [0]*128
     i = 0
-    flag = False
     time = -(resolution // 16)
     while i < len(result):
         if result[i] != " ":
             time += (resolution // 16)
-            while result[i] != " ":
-                flag = True
-                note = ord(result[i])
-                if repeat[note] == 0:
-                    print("2, " + str(time) + ", " + noteON + str(note) + ", 100")
-                    final.writelines("2, " + str(time) + ", " + noteON + str(note) + ", 100\n")
-                    repeat[note] = 1
+            j = 0
+            while j < len(result[i]):
+                note = ord(result[i][j])
+                if result[i].count(chr(note)) < 2:
+                    if repeat[note] == 0:
+                        print("2, " + str(time) + ", " + noteON + str(note) + ", 100")
+                        final.writelines("2, " + str(time) + ", " + noteON + str(note) + ", 100\n")
+                        repeat[note] = 1
+                    else:
+                        print("2, " + str(time) + ", " + noteOFF + str(note) + ", 64")
+                        final.writelines("2, " + str(time) + ", " + noteOFF + str(note) + ", 64\n")
+                        repeat[note] = 0
                 else:
-                    print("2, " + str(time) + ", " + noteOFF + str(note) + ", 64")
-                    final.writelines("2, " + str(time) + ", " + noteOFF + str(note) + ", 64\n")
-                    repeat[note] = 0
-                i += 1
+                    if repeat[note] == 0:
+                        print("2, " + str(time) + ", " + noteOFF + str(note) + ", 100")
+                        final.writelines("2, " + str(time) + ", " + noteOFF + str(note) + ", 100\n")
+                        repeat[note] = 1
+                    else:
+                        print("2, " + str(time) + ", " + noteON + str(note) + ", 64")
+                        final.writelines("2, " + str(time) + ", " + noteON + str(note) + ", 64\n")
+                        repeat[note] = 0
+
+                j += 1
         else:
             time += (resolution // 16)
-        if flag == False:
-            i += 1
-        else:
-            flag = False
+        i += 1
 
-    final.writelines("2, " + str(time - (resolution // 16)) + ", End_track\n")
+    final.writelines("2, " + str(time) + ", End_track\n")
     final.writelines("0, 0, End_of_file\n")
     final.close()
 
--- TxtToNoteConverterV2.py
+++ TxtToNoteConverterV2.py
@@ -4,8 +4,8 @@ import subprocess
 def max_val(channels):
     maxval = 0
     for i in range(len(channels)):
-        if maxval < int(channels[i][-1][1]):
-            maxval = int(channels[i][-1][1]) + 1
+        if maxval < int(channels[i][1]):
+            maxval = int(channels[i][1]) + 1
     return maxval
 
 
@@ -17,7 +17,6 @@ def txt_filter(file_name):
         j = 0
         resolution = round(int(data[i].split(", ")[5]) / 8)
         tracks = []
-        channels = []
         while i < len(data):
             if data[i].find("Start_track") != -1:
                 while data[i].find("End_track") == -1:
@@ -27,28 +26,25 @@ def txt_filter(file_name):
                         params[1] = str(round(int(params[1]) / resolution))  # time
                         tracks.append(params)
                     i = i + 1
-                channels.append(tracks)
             i = i + 1
-        max_time = max_val(channels)
+        max_time = max_val(tracks)
         result = []
         for i in range(max_time):
             result.append(" ")
 
-        for i in range(len(channels)):
-            for j in range(len(channels[i])):
-                params = channels[i][j]
-                print(params)
-                if result[int(params[1])].find(chr(int(params[4]))) == -1:
-                    result[int(params[1])] = result[int(params[1])].strip()
-                    result[int(params[1])] += chr(int(params[4]))
+        for row in tracks:
+            params = row
+            if result[int(params[1])].count(params[4]) < 3:
+                result[int(params[1])] = result[int(params[1])].strip()
+                result[int(params[1])] += chr(int(params[4]))
 
-        i = 0
         out = ""
         for i in range(len(result)):
             out += result[i]+" "
+        print(out)
     with open("txt_outputs\\note_" + file_name, "w") as of:
         of.write(out)
-    print(result)
+
 
 
 txt_files = os.listdir(os.getcwd() + "/txt_inputs")
